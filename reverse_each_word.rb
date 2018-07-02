64d3d63dffc00d28cd07d3661556a139990b4e2f
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-02 06:44:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 
 def reverse_each_word(sentence)
+  
   sentence_array = sentence.split(" ")
   
-  sentence_array.collect {|word|
-    word.reverse!}
-  
+  sentence_array.collect {|word| word.reverse!}
   
   sentence_array.join(" ")
   
